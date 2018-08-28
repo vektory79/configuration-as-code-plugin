5367cd363880459416e1b371debcf9aaac9193eb
Victor Verbitsky <vektor@krista.ru>
2018-08-28 10:02:47 +0300

    Replace star import to sequence of imports

--- plugin/src/main/java/io/jenkins/plugins/casc/BaseConfigurator.java
+++ plugin/src/main/java/io/jenkins/plugins/casc/BaseConfigurator.java
@@ -24,7 +24,14 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.lang.reflect.WildcardType;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.FINER;
